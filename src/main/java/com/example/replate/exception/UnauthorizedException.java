3cfbcc70a0049964bbcdaf4e0a39afe30faffb08
gaurijainn_ <gaurijainn@gmail.com>
2026-04-28 01:24:19 +0530

    UnauthorizedException added

--- src/main/java/com/example/replate/exception/UnauthorizedException.java
+++ src/main/java/com/example/replate/exception/UnauthorizedException.java
@@ -1,4 +1,9 @@
 package com.example.replate.exception;
 
-public class UnauthorizedException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.FORBIDDEN)
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) { super(message); }
 }
